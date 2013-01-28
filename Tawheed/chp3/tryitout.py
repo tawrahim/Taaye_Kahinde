f47837f9625eba9133c44173a57091bdd64e04a1
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-01-27 19:11:17 -0500

    Chp3 homework in Tawheed folder

---
+++ Tawheed/chp3/tryitout.py
@@ -0,0 +1,25 @@
+# Tawheed Abdul-Raheem
+# Chapter 3 Home work
+
+
+tip = (35.27 * 0.15)
+bill = (tip + 35.27) / 3
+print "Each person has to pay: "
+print bill
+
+area = 12.5 * 16.7
+print "The area is: "
+print area
+
+perimeter = 2 * (12.5 + 16.7)
+print "The perimeter is"
+print perimeter
+
+faranheit = 75
+C = 5.0/9 * (faranheit - 32)
+print "Temprature is:"
+print C
+
+distance = 200/80.0
+print "The distance is: "
+print distance
---
+++ Tawheed/input.py
@@ -0,0 +1,9 @@
+print "Enter a number"
+teacher = input()
+print "This is the teacher variable"
+print teacher
+
+if teacher == 44:
+    print "Good Job"
+else:
+    print "Bad job"
