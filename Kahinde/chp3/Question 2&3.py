f77b83e290d4d4c996435244013b256002d88072
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-02-17 20:18:10 -0500

    Kahide added some stuffs, Taaye also did some new change

---
+++ Kahinde/chp3/Question 2&3.py
@@ -0,0 +1,41 @@
+print "Question 2 in python chapter 3, (Try it out):"
+F=57.0
+C=5.0/9*(F-32.0)
+f="5.0/9*(F-32.0)"
+print "The number that we're using is..."
+print F
+print "This is the formula:"
+print f
+print "The answer is.."
+print C
+print "This is what you get when 57 degrees Fahrenheit"
+print "is converted to Celsius"
+print "Done by Kahinde Giwa"
+print "\n\n\n\n\n\n\n\n"
+print "Question 3 in python chapter 3, (Try it out):"
+l= "Km/hour"
+time= "minutes"
+S=80.0 
+D=200.0  
+f= "Travel Time=Distance/Speed"
+do= D/S
+print "If I was traveling somewhere,"
+print "and I wanted to know the time"
+print "it would take me, This is what"
+print "I would do:"
+print " So,The distance is..."
+print D ,l
+print "And the speed is..."
+print S,l
+print "Since what we're trying to find"
+print "is the travel time, we need to do"
+print "the formula"
+print "This is the formula"
+print f
+print "So the answer is..."
+print do ,time
+print "This is what you get for travel time."
+print "Done by Kahinde Giwa"
+
+
+
--- Tawheed/chp6/tryitout.py
+++ Tawheed/chp6/tryitout.py
@@ -1,8 +1,4 @@
-# Try changing the temperature-conversion program from chapter 5 to use GUI input and output instead of raw_input() and print.
-#Write a program that asks for your name, then house number, then street, then city, then province/territory/state, then postal/zip code (all in EasyGui dialog boxes).
-#The program should then display a mailing-style full address that looks something like this:
-
-
+# Question 1 
 purchasePrice = float(raw_input("What is the purchase price: "))
 if purchasePrice < 10:
 	discount = purchasePrice * .10
@@ -13,8 +9,11 @@ elif purchasePrice > 10:
 	amount = purchasePrice - discount
 	print "You got a 20% discount of ", discount, " and you are required to pay ", amount
 
-gender = raw_input("What is your gender: ")
+print "\n\n\n"
 
+
+# Question 2
+gender = raw_input("What is your gender: ")
 if gender == "f":
 	age = int(raw_input("How old are you: "))
 	if 10 <= age <= 12:
@@ -23,3 +22,12 @@ if gender == "f":
 		print "sorry you cant play on the team"
 else:
 	print "Sorry you need to be a female to be eligible for the team"
+
+print "\n\n\n"
+
+
+# Question 3
+size_of_tank = float(raw_input("Size of tank: "))
+percent_full = float(raw_input("Percent full: "))
+km_per_liter = float(raw_input("km per liter: "))
+
