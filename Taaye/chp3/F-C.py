40be991dfbcde7773667701ca94eb99c27e7d011
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-02-17 20:16:48 -0500

    Kahide added some stuffs, Taaye also did some new change

--- Taaye/chp3/F-C.py
+++ Taaye/chp3/F-C.py
@@ -6,15 +6,7 @@ print " We are going to convert it to Celsius"
 print "The answer is......"
 print 5.0/9.0*(F-32.0)
 print WD
-print '                                 '
-print '                                 '
-print '                                 '
-print '                                 '
-print '                                 '
-print '                                 '
-print '                                 '
-print '                                 '
-print '                                 '
+print '\n\n\n\n'
 
 
 
@@ -25,13 +17,9 @@ SP= 80
 F= 'Travel time= distance/speed'
 print 'question #2'
 print 'I want to figure how long it will take to go to Boston.'
-print 'The formula is,'
-print F
-print 'To find the answer I must divide the distance by the speed.'
-print 'Since the distance is,'
-print D
-print 'and the speed is,'
-print SP
+print 'The formula is,', F , 'To find the answer I must divide the distance by the speed.'
+print 'To find the answer I must divide the distance by the speed. Since the distance is,', D
+print 'and the speed is,', SP
 print 'I must divide them'
 T=D/SP
 print 'So the travel time is.....'
