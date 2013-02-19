fd2aafc86a98a5b1701eb94a8f75027cd4258937
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-02-18 22:02:12 -0500

    Kahinde completed first part of chapter4 assignment but had some problems

---
+++ Kahinde/chp4/Chapter 4 try.it.out questions.py
@@ -0,0 +1,11 @@
+print "This is an example of a string being"
+print "converted to a float"
+string= "12.34" 
+formula="float(string)"
+doit= float(string)
+string= "\"12.34\"" 
+print "The string is," ,string
+print "This is the formula:"
+print formula
+print "This is the answer:"
+print doit
