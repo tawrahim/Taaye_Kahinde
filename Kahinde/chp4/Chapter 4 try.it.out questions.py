2cb4cd1db3b40589408cdfeb488024ddd31e17f9
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-02-18 22:04:50 -0500

    I made some adjustment to her work by adding a bunch of comments to help clarify some basics

--- Kahinde/chp4/Chapter 4 try.it.out questions.py
+++ Kahinde/chp4/Chapter 4 try.it.out questions.py
@@ -1,10 +1,21 @@
 print "This is an example of a string being"
 print "converted to a float"
+
+#Ok so here, you are saving the string '12.34' into the variable string
 string= "12.34" 
+
+# When you get here you are just saving the formula to another variable called formula, remember that it is a string
 formula="float(string)"
+
+# This is where the magic happens, here you convert the string to a float and then save the converted answer to a variable called doit
 doit= float(string)
-string= "\"12.34\"" 
+
+# So here, I see that you are trying to print the string "12.34" but you said it is not working the way you want it to, because it is not printing it with the "". Well unfortuately that is not going to work because when you print a string, the quotation marks get ignored. To try out what I am saying, open up Idle => type this in name = "Kahinde" => now do print name, does the quaotation marks get printed when the name was printed? The answer is no! print simply ignores the quotation marks.
 print "The string is," ,string
+
+# If you want it to work, you have to do trick python by telling it that, you want a quotation to be printed around the variable. I think it is a little bit advance for now. The way you have it is fine and perfect. In any case if you are curious to find out how it is done, the line below demonstrates it.
+print "The string is," , "\"" , string , "\""
+
 print "This is the formula:"
 print formula
 print "This is the answer:"
