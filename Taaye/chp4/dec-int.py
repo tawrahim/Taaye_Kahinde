adabd47244aebf1e4a10ba64c0ea9026e77d0f51
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-02-19 20:38:23 -0500

    Taaye completed her assignment for chapter, I spotted a bug in her code so need to talk to her about it

---
+++ Taaye/chp4/dec-int.py
@@ -0,0 +1,44 @@
+print "Question #1, Try it out"
+print"\n"
+print'This is a way to change a string to a float'
+string1=12.34
+string="'12.34'"
+print 'The string is,',string
+point= '12.34'
+formula= float(string1)
+print "And the float is,",point
+
+print 'We use this formala to change a string into a float, the formula is,', formula
+print 'So then you get,'
+print formula
+print "\n"
+
+
+print 'Question #2, try it out'
+print "\n"
+formula= "int(56.78)"
+working=int(56.78)
+decimal=56.78
+
+print 'We are going to change the decimal,',decimal,'to an integer(number)'
+print 'To do that we must use the formula,',formula
+print 'Ten once once you type it in, you get the answer.......'
+print working
+print 'That is how you do it.'
+print '\n\n'
+
+
+print 'Question #3, try it out'
+print '\n'
+string="'1234.56'"
+string1=1234.56
+formula=int(string1)
+formula2= "('1234.56')"
+print 'To make the string,',string,'into an integer, we must use the formula , int ().'
+print 'But since you can not put quotations inside the perantheses,' ,formula2,'or it will not work.'
+print 'We have to assign,',string,'to a variable, any variable for example I will do number.'
+print 'Number=',string,'so now we can do, int(number).'
+print 'Then, once you type that in, you get............'
+print formula
+print 'That is how you do it'
+print 'Done by Taaye' 
