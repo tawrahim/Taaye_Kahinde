01ebf6cf299838317e5147c301e15f02cead2363
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-02-24 15:48:05 -0500

    Taaye and Kahinde finished a sample application after reading chapter5 to test their knowledge.

---
+++ Taaye/chp5/logic.py
@@ -0,0 +1,14 @@
+somebody=raw_input("Enter your name please:")
+print "My",
+print "Name",
+print "is",
+print "Taaye"
+print "\n"
+
+
+print "type a number you would like 1-5", somebody
+number = int(raw_input())
+if number != 3:
+      print "Sorry that is not the number I was thinking of."
+else:
+      print "Yah you got the number I was thinking of!"
